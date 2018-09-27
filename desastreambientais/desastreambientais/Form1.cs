3e658baba149a80048fdf9747c6b541f7541c697
Thomás Finholt <36731030+tfinholt10@users.noreply.github.com>
2018-09-27 10:53:32 -0300

    Add files via upload

---
+++ desastreambientais/desastreambientais/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desastreambientais
+{
+    public partial class Form1 : Form
+    {
+        private DAL _banco = new DAL();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rsbtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+
+}
---
+++ desastreambientais/desastreambientais/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace desastreambientais
+{   
+    class DAL
+    {
+        #region Atributos
+        private MySqlConnection _conexao;
+        private MySqlCommand _comandoDb;
+        private MySqlDataAdapter _adaptador;
+        private DataSet _dados;
+        #endregion
+
+        #region Getters and Setters
+        public string Host { get; set; } = "localhost";
+        public string DBNome { get; set; } ="";
+        public string Usuario { get; set; } ="root";
+        public string Senha { get; set; } = "";
+        #endregion
+
+
+        #region Construtor
+        public DAL() { }
+        #endregion
+
+        #region Metodos
+        public void Conectar()
+        {
+            //dados da conexão
+            //servidor, nome do banco, usuario e senha
+            //Server=localhost;Database=;Uid=root; Senha=
+            string strConexao = "Server=" + Host;
+            strConexao += ";Database=" + DBNome;
+            strConexao += ";Uid=" + Usuario;
+            strConexao += ";Pwd=" + Senha;
+            try
+            {
+                //Bloco de comandos sujeitos a erros
+                _conexao = new MySqlConnection(strConexao);
+                _conexao.Open();
+
+            }
+            catch(MySqlException erro)
+            {
+                //Captura o erro, caso ocorra
+                throw new Exception(erro.Message);
+
+            }
+            finally
+            {
+                //Libera o recurso, caso necessário, mesmo no erro
+                _conexao.Dispose();
+
+            }
+        }
+        public void Inserir(string sql) { }
+        public void Buscar() { }
+        public void Atualizar() { }
+        public void Excluir() { }
+        #endregion
+
+    }
+    
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    
+}
